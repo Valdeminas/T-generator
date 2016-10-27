3859ee0bd3d5030080c96138ac06514fb459f46a
Benas Kikutis <Benas.Kikutis@bentley.com>
2016-10-27 18:18:37 +0300

    Migration fix

---
+++ oldSolutionCopy/T-generator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(T_generator.Startup))]
+namespace T_generator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
