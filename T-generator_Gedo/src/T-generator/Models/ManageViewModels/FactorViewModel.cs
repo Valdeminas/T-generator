8b4c5dbb7607d468ba19885d7875e5fa037c7ca3
Gediminas Ziezys <vilkabrolis@gmail.com>
2016-10-29 17:58:44 +0300

    Renamed T-generator -> T-generator_Beno. Added Core project T-generator_Gedo.

---
+++ T-generator_Gedo/src/T-generator/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace T_generator.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
