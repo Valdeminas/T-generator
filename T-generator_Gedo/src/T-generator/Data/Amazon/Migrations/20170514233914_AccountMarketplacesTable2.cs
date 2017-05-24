381c1edc65a14c80d019f1b778b0561f76b7c50b
Gediminas Ziezys <vilkabrolis@gmail.com>
2017-05-24 12:04:02 +0300

    Benas back

---
+++ T-generator_Gedo/src/T-generator/Data/Amazon/Migrations/20170514233914_AccountMarketplacesTable2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tgenerator.Migrations.Amazon
+{
+    public partial class AccountMarketplacesTable2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.CreateTable(
+                name: "AccountMarketplaces",
+                columns: table => new
+                {
+                    MarketplaceID = table.Column<int>(nullable: false),
+                    AccountID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountMarketplaces", x => new { x.MarketplaceID, x.AccountID });
+                    table.ForeignKey(
+                        name: "FK_AccountMarketplaces_AmazonMarketplace_MarketplaceID",
+                        column: x => x.MarketplaceID,
+                        principalTable: "AmazonMarketplace",
+                        principalColumn: "AmazonMarketplaceID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AccountMarketplaces_AmazonAccount_AccountID",
+                        column: x => x.AccountID,
+                        principalTable: "AmazonAccount",
+                        principalColumn: "AmazonAccountID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountMarketplaces_MarketplaceID",
+                table: "AccountMarketplaces",
+                column: "MarketplaceID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountMarketplaces_AccountID",
+                table: "AccountMarketplaces",
+                column: "AccountID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+                 migrationBuilder.DropTable(
+                name: "AccountMarketplaces");
+                }
+    }
+}
